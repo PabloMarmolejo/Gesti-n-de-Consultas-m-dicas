44fd525547c47f925aadf12910a1977d43b039fc
Pablo <black7.devs@gmail.com>
2024-08-27 15:58:28 -0600

    redireccionamiento al index

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Gestión de Consultas</a>
+    <a class="navbar-brand" href="index.php">Gestión de Consultas</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
